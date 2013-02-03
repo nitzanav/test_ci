5f80c57c59e55c6d19145e61ed30880600ea2197
nitzan aviram <nitzan.aviram@naturalint.com>
2013-02-03 15:07:46 +0200

    New rails app with dummy rspec tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'c001f243b7c5b2b0b0343fcc840d2afd90931e27c01e2ec0fb5f635425b2d8f9c26fd6e365555717dd727e319520574f0cf0a6d4a33b4d168a0b7ceab70ebac4'
---
+++ spec/lib/dummy_test_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "dummy_test" do
+
+  describe "dummy_test" do
+    it "should equal but not" do
+      1.should == 0
+    end
+
+    it "should equal and is", :slow => true do
+      1.should == 1
+    end
+
+  end
+
+end
