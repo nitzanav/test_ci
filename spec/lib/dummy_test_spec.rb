e18c87be9736d317b7cfcf42d8147d29c5a0f7bd
nitzan aviram <nitzan.aviram@naturalint.com>
2013-02-03 15:21:26 +0200

    fix tests

--- spec/lib/dummy_test_spec.rb
+++ spec/lib/dummy_test_spec.rb
@@ -4,7 +4,7 @@ describe "dummy_test" do
 
   describe "dummy_test" do
     it "should equal but not" do
-      1.should == 0
+      1.should == 1
     end
 
     it "should equal and is", :slow => true do
